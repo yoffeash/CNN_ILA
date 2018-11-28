8b8929ca390b22cee1b01ac5feebb4134ac925b2
sya2 <sya2@phs007736.partners.org>
2018-11-28 15:34:29 -0500

    updated genetic analysis

--- R/08_genetic.R
+++ R/08_genetic.R
@@ -11,3 +11,4 @@ summary(muc5_lh_nhw_fit)
 ## CNN NHW
 muc5_cnn_nhw_fit <- lm(pct_ild_cnn ~ muc5 + ccenter + gender + SmokCigNow + FEV1pp_utah + BMI + pctEmph_Slicer, data=final_data_nhw)
 summary(muc5_cnn_nhw_fit)
+
